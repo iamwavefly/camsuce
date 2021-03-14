3555f8c8cd9cece2857a7ba9a6aa302a04318b3f
wavefly <iamwavefly@gmail.com>
2021-03-15 00:52:09 +0100

    ipconfig endpoint change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Join from './component/Join'
 function App() {
   const [userCity, setUserCity] = useState('nearby')
   const locateUser = () => {
-    Axios.get('https://ip-api.com/json/')
+    Axios.get('http://api.ipstack.com/check?access_key=56043ed5a2342068e55487cf4c4e7002')
     .then(data => {
       const userCurrentCity = data.data.city
       setUserCity(userCurrentCity)
