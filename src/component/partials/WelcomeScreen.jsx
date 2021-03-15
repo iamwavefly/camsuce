80ad888bd1b6fec7e971b9a354a70df1a7ed0646
wavefly <iamwavefly@gmail.com>
2021-03-15 12:42:12 +0100

    update sign up page

--- src/component/partials/WelcomeScreen.jsx
+++ src/component/partials/WelcomeScreen.jsx
@@ -4,11 +4,12 @@ import '../../styles//welcome__screen.css';
 function WelcomeScreen({ userCity }) {
   return (
     <div className="welcome__screen">
-      <h1>MEET {userCity.toUpperCase()} SINGLES NOW!</h1>
+      <h1>MEET {userCity.toUpperCase()} SINGLES!</h1>
       <p>
         If you're shy and single, we know your type and we got you covered,
         let's connect you with {userCity} singles now.
       </p>
+      <a href="mailto:iamwavefly@gmail.com">CONTACT US</a>
     </div>
   );
 }
